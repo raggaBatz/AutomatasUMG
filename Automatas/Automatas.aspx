1561c58d256eeb400f509454f4d30dba7ffb85cf
raggabatz <erik6594@gmail.com>
2021-10-26 18:10:38 -0600

    init repo

---
+++ Automatas/Automatas.aspx
@@ -0,0 +1,124 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Automatas.aspx.cs" Inherits="Automatas.Automatas" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        
+          <a class="navbar-brand" href="#">AUTOMATAS</a>
+        <ul class="nav navbar-nav">
+          
+        </ul>
+      
+    </nav>
+    <div id="main">
+        <section class="container">
+            <div class="container">
+                <form id="form1" runat="server">
+                    <br />
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" id="file" lang="es" runat="server"/>
+                                <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <button type="button" class="btn btn-success" runat="server" onserverclick="cargar">Cargar archivo</button>
+                        </div>
+                    </div>
+                    <br />
+                    
+                    <div class="row" id="divuno" runat="server" visible="false">
+                        <div class="col-sm-3">
+                            <div class="md-form">
+                                <i class="fa fa-file-o"></i>
+                                <label>TXT</label>
+                                <textarea id="areaArchivo" runat="server" class="md-textarea form-control" rows="10" readonly="readonly" style="resize: none;"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="md-form">
+                                <i class="fa fa-quora"></i>
+                                <label>Q</label>
+                                <textarea id="areaQEstados" runat="server" class="md-textarea form-control" rows="10" readonly="readonly" style="resize: none;"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="md-form">
+                                <i class="fa fa-facebook"></i>
+                                <label>Σ</label>
+                                <textarea id="areaFAlfabeto" runat="server" class="md-textarea form-control" rows="10" readonly="readonly" style="resize: none;"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="md-form">
+                                <i class="fa fa-font"></i>
+                                <label>A</label>
+                                <textarea id="areaAAceptacion" runat="server" class="md-textarea form-control" rows="10" readonly="readonly" style="resize: none;"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="md-form">
+                                <i class="fa fa-wikipedia-w"></i>
+                                <label>W = AFN</label>
+                                <asp:Panel ID="panel1" runat="server" Height="285px" Width="100%" ScrollBars="Vertical">
+                                    <asp:GridView ID="GridViewAFN" runat="server" Width="100%" CssClass="table table-striped table-bordered table-hover" AutoGenerateColumns="True" DataKeyNames="N" EmptyDataText="There are no data records to display.">
+                                    <%--<HeaderStyle CssClass="thead-custom"/>--%>
+                                </asp:GridView>
+                                </asp:Panel>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row" id="divdos" runat="server" visible="false">
+                        <div class="col-sm-5">
+                            <div class="md-form">
+                                <i class="fa fa-wikipedia-w"></i>
+                                <label>W = AFD</label>
+                                <asp:Panel ID="panel2" runat="server" Height="285px" ScrollBars="Vertical">
+                                    <%--<asp:GridView ID="GridViewAFD" runat="server" Width="100%" CssClass="table table-striped table-bordered table-hover" AutoGenerateColumns="True" DataKeyNames="ESTADO" EmptyDataText="There are no data records to display.">
+                                        <HeaderStyle CssClass="thead-custom"/>
+                                    </asp:GridView>--%>
+                                    <asp:GridView ID="GridViewAFD" runat="server" Width="100%" CssClass="table table-striped table-bordered table-hover" AutoGenerateColumns="True" DataKeyNames="N" EmptyDataText="There are no data records to display.">
+                                        <HeaderStyle CssClass="thead-custom"/>
+                                    </asp:GridView>
+                                </asp:Panel>
+                            </div>
+                        </div>
+                        <%--<div class="col-sm-2">
+                            <div class="md-form">
+                                <i class="fa fa-file-o"></i>
+                                <label>Quintupla</label>
+                                <textarea id="taQuintupla" runat="server" class="md-textarea form-control" rows="10" readonly="readonly"></textarea>
+                            </div>
+                        </div>--%>
+                        <div class="col-sm-2">
+                            <div class="md-form">
+                                <asp:TextBox ID="tbxCadena" runat="server" CssClass="form-control input-lg" Width="100%" TabIndex="1" placeholder="Cadena"/>
+                            </div>
+                        </div>
+                        <%--<div class="col-sm-3">
+                            <div class="md-form">
+                                <label>.</label>
+                                <button type="button" class="btn btn-info" id="btnValidarCadena" runat="server" onserverclick="btnValidarCadena_Click">Validar cadena</button>
+                            </div>
+                        </div>--%>
+                    </div>
+
+                </form>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
---
+++ Automatas/Automatas.aspx.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Automatas
+{
+    public partial class Automatas : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void cargar(object sender, EventArgs e)
+        {
+            divuno.Visible = true;
+            divdos.Visible = true;
+            string carpeta = "~/archivos/";
+            if (file.PostedFile != null && file.PostedFile.ContentLength > 0)
+            {
+                //Se valida que exista carpeta y si no existe se crea
+                if (!Directory.Exists(Server.MapPath(carpeta)))
+                    Directory.CreateDirectory(Server.MapPath(carpeta));
+                //Guarda el archivo en carpeta archivos
+                file.PostedFile.SaveAs(String.Concat(Server.MapPath(carpeta), file.PostedFile.FileName));
+                //Convierte cadena de archivo en arreglo por linea
+                String[] arregloArchivo = File.ReadAllLines(Server.MapPath(carpeta) + file.PostedFile.FileName);
+                arregloArchivo = arregloArchivo.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+                //Manipulacion de informacion
+                Manipular_Data(arregloArchivo);
+            }
+        }
+        private void Manipular_Data(String[] arregloArchivo) {
+            //Se obtienen las cadenas de acuerdo a la letra
+            String cadenaQ = arregloArchivo[0];
+            String cadenaF = arregloArchivo[1];
+            String cadenaI = arregloArchivo[2];
+            String cadenaA = arregloArchivo[3];
+            String cadenaW = arregloArchivo[4];
+            //Se llena area donde se muestra el contenido del archivo
+            //Q
+            areaArchivo.InnerText = cadenaQ + Environment.NewLine;
+            //F
+            areaArchivo.InnerText += cadenaF + Environment.NewLine;
+            //I
+            areaArchivo.InnerText += cadenaI + Environment.NewLine;
+            //A
+            areaArchivo.InnerText += cadenaA + Environment.NewLine;
+            //W
+            areaArchivo.InnerText += cadenaW + Environment.NewLine;
+            //Se limpian las cadenas, se obtienen unicamente los valores a trabajar
+            cadenaQ = cadenaQ.Substring((cadenaQ.IndexOf("{") + 1), cadenaQ.IndexOf("}") - (cadenaQ.IndexOf("{") + 1));
+            cadenaF = cadenaF.Substring((cadenaF.IndexOf("{") + 1), cadenaF.IndexOf("}") - (cadenaF.IndexOf("{") + 1));
+            cadenaI = cadenaI.Substring(cadenaI.IndexOf(":") + 1, 1);
+            cadenaA = cadenaA.Substring((cadenaA.IndexOf("{") + 1), cadenaA.IndexOf("}") - (cadenaA.IndexOf("{") + 1));
+            cadenaW = cadenaW.Substring((cadenaW.IndexOf("{") + 1), cadenaW.IndexOf("}") - (cadenaW.IndexOf("{") + 1));
+            //se convierten las cadenas de valores en arreglos
+            String[] arregloQ = cadenaQ.Split(',');
+            String[] arregloF = cadenaF.Split(',');
+            String[] arregloW = cadenaW.Split(';');
+            //Removiendo espacios en blanco
+            arregloQ = arregloQ.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            arregloF = arregloF.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            arregloW = arregloW.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            //Llenando area Q estados
+            areaQEstados.InnerText = "";
+            foreach (string valor in arregloQ)
+            {
+                areaQEstados.InnerText += valor + Environment.NewLine;
+            }
+            //Llenando area F alfabeto
+            areaFAlfabeto.InnerText = "";
+            foreach (string valor in arregloF)
+            {
+                areaFAlfabeto.InnerText += valor + Environment.NewLine;
+            }
+            //Llenando area A aceptacion
+            areaAAceptacion.InnerText = cadenaA;
+            //Se valida si existe epsilon
+            bool banderaEpsilon = false;
+            if (cadenaW.Contains("e")) {
+                banderaEpsilon = true;
+            }
+            //Se llena tabla AFN
+            DataTable tablaAFN = new DataTable();
+            tablaAFN = generarAFN(arregloW, arregloF,banderaEpsilon);
+            GridViewAFN.DataSource = tablaAFN;
+            GridViewAFN.DataBind();
+            //Se llena tabla AFD
+            DataTable tablaAFD = new DataTable();
+            //tablaAFD = generarAFD(tablaAFN, cadenaI, arregloF, banderaEpsilon);
+            GridViewAFD.DataSource = tablaAFN;
+            GridViewAFD.DataBind();
+            //Se arma quintupla
+            //armarQuintuplaAFD(A, AFD);
+            //Session["GlobalAFD"] = AFD;
+
+        }
+
+        private DataTable generarAFN(String[] arregloW, String[] arregloF, bool banderaEpsilon)
+        {
+            DataTable tablaAFN = new DataTable();
+            //Se arma el encabezado de tabla
+            tablaAFN.Columns.Add("N");
+            //Se recorre el alfabeto para crear las columnas correspondientes
+            foreach (string letra in arregloF)
+            {
+                tablaAFN.Columns.Add(letra);
+            }
+            //Si existe epsilon se agrega columna epsilon
+            if (banderaEpsilon)
+            {
+                tablaAFN.Columns.Add("e");
+            }
+            //Se recorre la tabla de transicion
+            foreach (String estadoTran in arregloW)
+            {
+                String transicion = estadoTran;
+                //Eliminando parentesis de linea
+                transicion = transicion.Replace("(","");
+                transicion = transicion.Replace(")", "");
+                //Se crea arreglo de valores, separandolos por coma
+                //estado inicial, alfabeto, estado destino
+                String[] valores = transicion.Split(',');
+                bool bandera = false;
+                //Se recorre tabla para validar si estado ya existe
+                foreach (DataRow fila in tablaAFN.Rows)
+                {
+                    //Si estado inicial ya existe en tabla, se activa la bandera indicando que ya existe
+                    if (valores[0].Equals(fila["N"]))
+                    {
+                        bandera = true;
+                        //Concatena valor
+                        if (fila[valores[1]].ToString().Equals(""))
+                            fila[valores[1]] = valores[2];
+                        else
+                            fila[valores[1]] += "," + valores[2];
+                    }
+                }
+                //Si bandera false, es decir que el estado no existe
+                if (!bandera)
+                {
+                    //Lo agrega a la tabla
+                    DataRow filaTabla = tablaAFN.NewRow();
+                    filaTabla["N"] = valores[0];
+                    filaTabla[valores[1]] = valores[2];
+                    tablaAFN.Rows.Add(filaTabla);
+                }
+            }
+            return tablaAFN;
+        }
+
+        private DataTable generarAFD(DataTable tablaAFN, String inicial, String[] alfabeto, bool banderaEpsilon)
+        {
+            DataTable tablaAFD = new DataTable();
+            if (tablaAFN.Rows[0]["N"].Equals(inicial))
+            {
+                //Se arma encabezado de tabla AFN
+                tablaAFD.Columns.Add("ESTADO");
+                //Se recorre el alfabeto para crear las columnas correspondientes
+                foreach (string letra in alfabeto)
+                {
+                    tablaAFD.Columns.Add(letra);
+                }
+                //Se agrega columna composicion
+                tablaAFD.Columns.Add("COMPOSICION");
+
+                DataRow dr = tablaAFD.NewRow();
+                dr["ESTADO"] = "A";
+                String e = "";
+                if (banderaEpsilon) {
+                    e = cerraduraEpsilon(tablaAFN.Rows[0]["e"].ToString(), tablaAFN);
+                }
+                if (e.Equals(""))
+                {
+                    e = "0";
+                    dr["COMPOSICION"] = clean((tablaAFN.Rows[0]["N"].ToString()));
+                }
+                else
+                {
+                    dr["COMPOSICION"] = clean((tablaAFN.Rows[0]["N"].ToString() + "," + e));
+                }
+                tablaAFD.Rows.Add(dr);
+
+                tablaAFD = armarComposiciones(tablaAFD, tablaAFN);
+                tablaAFD = armarComposiciones(tablaAFD, tablaAFN);
+            }
+            return tablaAFD;
+        }
+
+        private DataTable armarComposiciones(DataTable AFD, DataTable AFN)
+        {
+            for (int i = 0; i < AFD.Rows.Count; i++)
+            {
+                foreach (DataColumn column in AFD.Columns)
+                {
+                    if (!column.ColumnName.Equals("ESTADO") && !column.ColumnName.Equals("COMPOSICION"))
+                    {
+                        String composicion = clean(remove(cerraduraEpsilon(move(column.ColumnName, AFN, AFD.Rows[i]["COMPOSICION"].ToString()), AFN)));
+                        if (composicion.Equals(""))
+                            composicion = "0";
+                        bool flag = false;
+                        string lastLetter = "";
+                        foreach (DataRow r in AFD.Rows)
+                        {
+                            if (r["COMPOSICION"].Equals(composicion))
+                            {
+                                flag = true;
+                                AFD.Rows[i][column.ColumnName] = r["ESTADO"].ToString();
+                            }
+                            lastLetter = r["ESTADO"].ToString();
+                        }
+                        if (!flag)
+                        {
+                            DataRow dr = AFD.NewRow();
+                            dr["ESTADO"] = nextLetter(lastLetter);
+                            dr["COMPOSICION"] = composicion;
+                            AFD.Rows.Add(dr);
+                        }
+                    }
+                }
+            }
+            return AFD;
+        }
+
+        private String remove(String texto)
+        {
+            String[] tmp = texto.Split(',');
+            tmp = tmp.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            tmp = tmp.Distinct().ToArray();
+
+            return string.Join(",", tmp);
+        }
+
+        private string nextLetter(String value)
+        {
+
+            char letter = value[0];
+            char next;
+
+            if (letter == 'z')
+                next = 'a';
+            else if (letter == 'Z')
+                next = 'A';
+            else
+                next = (char)(((int)letter) + 1);
+
+            return next.ToString();
+        }
+
+        private string move(String letra, DataTable AFN, String composicion)
+        {
+            string[] tmp = composicion.Split(',');
+            String cadena = "";
+            foreach (string item in tmp)
+            {
+                foreach (DataRow row in AFN.Rows)
+                {
+                    if (row["N"].Equals(item))
+                    {
+                        if (!row[letra].ToString().Equals(""))
+                            if (cadena.Equals(""))
+                                cadena = row[letra].ToString();
+                            else
+                                cadena += "," + row[letra].ToString();
+                    }
+                }
+            }
+
+            return cadena;
+        }
+
+        private String cerraduraEpsilon(String cadena, DataTable dt)
+        {
+
+            String[] aux;
+
+            if (cadena.Contains(","))
+                aux = cadena.Split(',');
+            else
+                aux = new string[1] { cadena };
+
+            foreach (string item in aux)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    if (dr["N"].Equals(item))
+                    {
+                        if (!dr["e"].ToString().Equals(""))
+                            if (cadena.Equals(""))
+                                cadena = cerraduraEpsilon(dr["e"].ToString(), dt);
+                            else
+                                cadena += "," + cerraduraEpsilon(dr["e"].ToString(), dt);
+                        break;
+                    }
+                }
+            }
+            return cadena;
+        }
+
+        private String clean(String text)
+        {
+            if (text.Contains(","))
+            {
+                string[] tmp = text.Split(',');
+                int quantity = tmp.Count();
+                int[] nums = new int[tmp.Length];
+
+                for (int i = 0; i < tmp.Length; i++)
+                {
+                    nums[i] = int.Parse(tmp[i]);
+                }
+
+                Comparison<int> comparador = new Comparison<int>((numero1, numero2) => numero1.CompareTo(numero2));
+                Array.Sort<int>(nums, comparador);
+                int pos = -1;
+                tmp = new string[quantity];
+                foreach (int numero in nums)
+                {
+                    pos++;
+                    tmp[pos] = numero.ToString();
+
+                }
+                return String.Join(",", tmp);
+            }
+            else
+            {
+                return text;
+            }
+        }
+    }
+}
